aa85de5a60e4d80f78b9a6ab6abdac4640e01c2f
fire7ptaszek <feniksmati@gmail.com>
2017-12-09 00:02:07 +0100

    Default Health after explosion

--- spacejam/Assets/Script/ObjectSpace.cs
+++ spacejam/Assets/Script/ObjectSpace.cs
@@ -3,35 +3,64 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ObjectSpace : MonoBehaviour {
+public class ObjectSpace : MonoBehaviour
+{
     public int health = 50; //usunąć public jak działa
+    public int explosionDamage = 20;
+    private const int defaultHealthAfterExplosion = 10;
     public Image objectConditionImage;
     public int precentPerTime = 3;
     public int timeBetweenLostCondition;
     float timer;
-    public int chanceToExplode=0;
+    public int chanceToExplode = 0;
     public GameObject explode;
-	// Use this for initialization
-	void Start () {
-        timer = timeBetweenLostCondition;	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        
-	}
+
+    public int Health
+    {
+        get
+        {
+            return health;
+        }
+        set
+        {
+            health = value;
+            if (health <= 0)
+            {
+                health = 0;
+            }
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        timer = timeBetweenLostCondition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
     private void FixedUpdate()
     {
         if (timer < 0)
         {
-            health -= precentPerTime;
+            Health -= precentPerTime;
             timer = timeBetweenLostCondition;
             if (Random.Range(0, 100) < chanceToExplode)
             {
                 Instantiate(explode, transform.position, transform.rotation);
-                health -= 20;
+                if (Health - explosionDamage <= 10)
+                {
+                    Health = defaultHealthAfterExplosion;
+                }
+                else
+                {
+                    Health -= explosionDamage;
+                }
+
             }
-            if (health < 0) health = 0;
         }
         else
         {
