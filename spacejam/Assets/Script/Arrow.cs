9e9f2c6bd07bd6f63f810b4c0aced71436689471
fire7ptaszek <feniksmati@gmail.com>
2017-12-08 23:07:43 +0100

    UnexpectedAction + script

---
+++ spacejam/Assets/Script/Arrow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour {
+
+    public bool intersect = false;
+    // Use this for initialization
+    public bool afterInter = false;
+    private void OnEnable()
+    {
+        afterInter = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("And");
+        if(collision.CompareTag("Placement"))
+        {
+            Debug.Log("Inter");
+            intersect = true;
+            afterInter = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Placement"))
+        {
+            if (!afterInter)
+            {
+                intersect = false;
+                Debug.Log("Out");
+            }
+        }
+    }
+}
---
+++ spacejam/Assets/Script/UnexpectedActionController.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class UnexpectedActionController : MonoBehaviour
+{
+
+    public GameObject background;
+    public GameObject arrow;
+    public GameObject goodPlacement;
+
+    public Transform startPoint;
+    public Transform endPoint;
+
+    public Arrow arrowCollision;
+
+    public float arrowSpeed;
+    public float fateAction;
+    private float actionTimer;
+
+    static bool isEventActionStart = false;
+    private bool enabledMomentActionStart = false;
+    private bool IsMomentActionStart
+    {
+        get
+        {
+            return enabledMomentActionStart;
+        }
+        set
+        {
+            enabledMomentActionStart = value;
+            background.SetActive(value);
+            arrow.SetActive(value);
+            goodPlacement.SetActive(value);
+        }
+    }
+    private bool isPlayerPressSpace = false;
+
+    Transform transformPlayer;
+
+    void Start()
+    {
+
+    }
+
+    void StartUnexpectedAction()
+    {
+        ResetTimerAction();
+        isEventActionStart = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if (isEventActionStart)
+        {
+            //Repair
+            RunTimerAction();
+        }
+
+        if(arrow.activeInHierarchy)
+        {
+            arrow.transform.Translate(arrowSpeed * Time.fixedDeltaTime, 0, 0);
+        }
+
+        if(arrow.transform.position.x>=endPoint.position.x)
+        {
+            IsMomentActionStart = false;
+        }
+    }
+
+    void RunTimerAction()
+    {
+        if (!IsMomentActionStart)
+            if (actionTimer <= 0f && !IsMomentActionStart)
+            {
+                actionTimer = fateAction;
+                ManageSpriteAction();
+                IsMomentActionStart = true;
+                ResetTimerAction();
+            }
+            else
+            {
+                actionTimer -= Time.fixedDeltaTime;
+            }
+    }
+
+    void ManageSpriteAction()
+    {
+        Debug.Log("Show");
+        arrow.transform.position = startPoint.transform.position;
+    }
+
+    void ResetTimerAction()
+    {
+        actionTimer = fateAction;
+    }
+
+    private void Update()
+    {
+        if (IsMomentActionStart)
+            CheckInputSpace();
+
+        if(Input.GetButtonDown("Submit"))
+        {
+            StartUnexpectedAction();
+            Debug.Log("GOGOGOGO");
+        }
+    }
+
+    private void CheckInputSpace()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            IsMomentActionStart = false;
+            CheckIfArrowHasGoodPlace();
+        }
+    }
+
+    void CheckIfArrowHasGoodPlace()
+    {
+
+        if(arrowCollision.intersect)
+        {
+            Debug.Log("IT WURKS");
+            arrowCollision.intersect = false;
+        }
+        else
+        {
+            Debug.Log("Wrong Idiot");
+        }
+        //arrow placement
+        IsMomentActionStart = false;
+    }
+
+    void StopUnexpectedAction()
+    {
+        isEventActionStart = false;
+        IsMomentActionStart = false;
+        isPlayerPressSpace = false;
+    }
+}
