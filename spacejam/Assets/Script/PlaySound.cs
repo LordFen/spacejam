9c11717bc4fd11e90c541cfb69fb4924696c4634
fire7ptaszek <feniksmati@gmail.com>
2017-12-09 14:48:00 +0100

    Random music

---
+++ spacejam/Assets/Script/PlaySound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour {
+
+    public AudioSource audioSource;
+    public AudioClip[] tabClips;
+	// Use this for initialization
+	void Start () {
+        PlayNextSong();
+	}
+
+    void PlayNextSong()
+    {
+        audioSource.Stop();
+        Debug.Log("NextSong");
+        audioSource.clip=tabClips[Random.Range(0, tabClips.Length)];
+        audioSource.Play();
+        Invoke("PlayNextSong", audioSource.clip.length);
+    }
+	
+
+}
