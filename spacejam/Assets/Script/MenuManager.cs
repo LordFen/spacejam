c6fe2d1fcd1b823d2650a0c45380e350da1e8aa0
enter24 <enter24@vp.pl>
2017-12-09 04:39:11 +0100

    no message

---
+++ spacejam/Assets/Script/MenuManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour {
+    public Button start;
+	// Use this for initialization
+	void Start () {
+        start.onClick.AddListener(StartGame);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void StartGame()
+    {
+        SceneManager.LoadScene("main");
+    }
+
+}
